3713ab7cca152b39230b0c5be1e222bb39a80b1f
zzwv <9934536+ZZWVV@user.noreply.gitee.com>
2023-03-24 18:03:34 +0800

    基本框架完成

---
+++ src/main/java/com/zzwv/ChinesePoetryApplication.java
@@ -0,0 +1,13 @@
+package com.zzwv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChinesePoetryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChinesePoetryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zzwv/ChinesePoetryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zzwv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChinesePoetryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
